6e0dd4f8c063ab94391526cc66f7ffcc87a6f03d
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-08-09 04:31:12 +0300

    feat:add elsint and prittier and rules for theme

--- react-chess/src/App.tsx
+++ react-chess/src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1>Hello World i finnaly start It!</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Hello World i finnaly start It!</h1>
+        </>
+    )
 }
 
-export default App;
+export default App
