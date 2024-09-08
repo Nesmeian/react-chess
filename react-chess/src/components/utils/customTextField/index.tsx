66fb84e490a2e47ea93f4d5e837e2c3de2c85fb9
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-09-08 03:57:13 +0300

    feat:change variant of textField in 768 and lower sizes

--- react-chess/src/components/utils/customTextField/index.tsx
+++ react-chess/src/components/utils/customTextField/index.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material'
+import { TextField, useMediaQuery } from '@mui/material'
 import { useFormContext } from 'react-hook-form'
 
 interface CustomTextFieldProps {
@@ -20,11 +20,12 @@ export default function CustomTextField({
         register,
         formState: { errors },
     } = useFormContext()
+    const isSmallScreen = useMediaQuery('(max-width:768px)')
     return (
         <TextField
             id={`${name}-input`}
             label={label}
-            variant="outlined"
+            variant={isSmallScreen ? 'filled' : 'outlined'}
             type={type}
             {...register(name)}
             error={!!errors[name]}
